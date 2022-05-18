dd8a90e46c5a64cb8117cb4bfb0fd861a43aa8f9
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2022-05-18 22:41:14 +0200

    02

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-new Vue({
-    el: '#app',
-    data () {
-      return {
-        info: null,
-        loading: true,
-        errored: false
-      }
-    },
-    mounted () {
-      axios
-        .get('https://ergast.com/api/f1/current/driverStandings.json')
-        .then(response => {
-          this.info = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-        })
-        .catch(error => {
-          console.log(error)
-          this.errored = true
-        })
-        .finally(() => this.loading = false)
-    }
-  })
+$.getJSON(
+  "https://ergast.com/api/f1/current/driverStandings.json",
+  function(data){
+    console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    var id0 = data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver.driverId;
+    var id1 = data.MRData.StandingsTable.StandingsLists[0].DriverStandings[1].Driver.driverId;
+    var id2 = data.MRData.StandingsTable.StandingsLists[0].DriverStandings[2].Driver.driverId;
+    var points1 = data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].points;
+    
+    var driver01= "./images/" + id0 + ".png";
+    var driver02= "./images/" + id1 + ".png";
+    var driver03= "./images/" + id2 + ".png";
+    console.log(points1);
+    $('.driver1').attr('src',driver01);
+    $('.driver2').attr('src',driver02);
+    $('.driver3').attr('src',driver03);
+    $('.points1').append(points1,);
+
+
+  }
+);
